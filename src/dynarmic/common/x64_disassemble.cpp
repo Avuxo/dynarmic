05c466275932305f84192c4f677218384d2b66fa
ben <Avuxo@users.noreply.github.com>
2021-09-07 20:40:13 -0400

    add function to return disassembly as vector of strings

--- src/dynarmic/common/x64_disassemble.cpp
+++ src/dynarmic/common/x64_disassemble.cpp
@@ -32,4 +32,30 @@ void DumpDisassembledX64(const void* ptr, size_t size) {
     }
 }
 
+// Disassemble `size' bytes from `ptr' and return the disassembled lines as a vector
+// of strings.
+std::vector<std::string> Dynarmic::Common::DisassembleX64(const void* ptr, size_t size) {
+    std::vector<std::string> result;
+    ZydisDecoder decoder;
+    ZydisDecoderInit(&decoder, ZYDIS_MACHINE_MODE_LONG_64, ZYDIS_ADDRESS_WIDTH_64);
+
+    ZydisFormatter formatter;
+    ZydisFormatterInit(&formatter, ZYDIS_FORMATTER_STYLE_INTEL);
+
+    size_t offset = 0;
+    ZydisDecodedInstruction instruction;
+    while (ZYAN_SUCCESS(ZydisDecoderDecodeBuffer(&decoder, (const char*)ptr + offset, size - offset, &instruction))) {
+        fmt::print("{:016x}  ", (u64)ptr + offset);
+
+        char buffer[256];
+        ZydisFormatterFormatInstruction(&formatter, &instruction, buffer, sizeof(buffer), (u64)ptr + offset);
+        
+        result.push_back(fmt::format("{:016x}  {}", (u64)ptr + offset, buffer));
+
+        offset += instruction.length;
+    }
+
+    return result;
+}
+
 }  // namespace Dynarmic::Common
--- src/dynarmic/common/x64_disassemble.h
+++ src/dynarmic/common/x64_disassemble.h
@@ -6,9 +6,12 @@
 #pragma once
 
 #include "dynarmic/common/common_types.h"
+#include <vector>
+#include <string>
 
 namespace Dynarmic::Common {
 
+std::vector<std::string> DisassembleX64(const void* ptr, size_t size);
 void DumpDisassembledX64(const void* ptr, size_t size);
 
 }  // namespace Dynarmic::Common
